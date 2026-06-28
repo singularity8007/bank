4202d16570e171e543e6fa7b9833455a5345950c
Mustansar Mehmood <mustansar.mehmood@gmail.com>
2026-06-28 12:36:43 -0500

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,26 +6,11 @@ from app.logging_config import logger
 
 load_dotenv()
 
-<<<<<<< HEAD
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://db_user:db%40user@localhost:5432/postgres")
 
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True shows SQL (good for learning)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-#Base = declarative_base()
-
-class Base(declarative_base()):
-    """Base class for all SQLAlchemy models."""
-
-    __abstract__ = True  # Prevents SQLAlchemy from creating a table for this class
-    __table_args__ = {"schema": "bank"}
-    
-
-=======
-DATABASE_URL = os.getenv("DATABASE_URL")
->>>>>>> simple-bank
-
 
 def get_connection():
     """Returns a new database connection"""
